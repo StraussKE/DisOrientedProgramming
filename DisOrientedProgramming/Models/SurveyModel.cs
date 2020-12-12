c27741d05d0eaec709a194162cbc818bd448dc32
Cole A Vanlandingham <cvanlan@Coles-MacBook-Pro.local>
2020-12-11 21:16:25 -0800

    Added MVC Components of Servey page.

---
+++ DisOrientedProgramming/Models/SurveyModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace DisOrientedProgramming.Models
+{
+    public class SurveyModel
+    {
+        public SurveyModel()
+        {
+        }
+
+        public AppUser User { get; set; }
+
+        public string Question1 { get; set; }
+
+        public string Question2 { get; set; }
+
+        public string Question3 { get; set; }
+
+        public string Question4 { get; set; }
+
+
+
+    }//End Class 
+}//End Namespace 
